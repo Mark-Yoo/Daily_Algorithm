1e089af8db1d5e7c5b2dfb4700cd4e98d032d96c
Mark Yoo <yjinhyuk@gmail.com>
2020-04-17 12:47:35 +0900

    Feat: Replace with alphabet position from codewars algorithm

---
+++ CodeWars/Replace_with_alphabet.js
@@ -0,0 +1,25 @@
+// Replace With Alphabet Position
+
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+// 알파벳의 순서를 숫자로 환산한 후, 인자로 받는 text의 각 글자에 해당하는 숫자로 바뀐 뒤 문자열로 만들어 return 해야 한다.
+
+function alphabetPosition(text) {
+  const alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const textToStr = text.toLowerCase().split('');
+  return textToStr.map(letter => (alphabets.indexOf(letter)) + 1).filter(num => num).join(' ');
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+console.log(alphabetPosition("The narwhal bacons at midnight.")); // "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
