28b8136dd91d61b1b58b938ab4797000d43fc810
Mark Yoo <yjinhyuk@gmail.com>
2021-06-17 23:29:00 +0900

    Add: am_i_hill.js

---
+++ am_i_hill.js
@@ -0,0 +1,16 @@
+// 2차원 배열 격자판에서 사방의 숫자보다 큰 숫자일 경우 이 숫자는 '봉우리'가 된다. 봉우의 갯수를 구하시오.
+
+function solution(arr) {
+  let answer = 0;
+
+  return answer;
+}
+
+const arr1 = [
+  [5, 3, 7, 2, 3],
+  [3, 7, 1, 6, 1],
+  [7, 2, 5, 3, 4],
+  [4, 3, 6, 4, 1],
+  [8, 7, 3, 5, 2],
+];
+console.log(solution(arr1));
