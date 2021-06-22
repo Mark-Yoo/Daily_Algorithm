e2baa595547ba3b5f7ffeaacbf92cf5a9ea6a5a9
Mark Yoo <yjinhyuk@gmail.com>
2021-06-22 18:08:59 +0900

    Solved: am_i_hill.js Detail: used 3 layers 'for' statement Detail2: will be on refactoring decrease number of 'for' statement use

--- am_i_hill.js
+++ am_i_hill.js
@@ -2,7 +2,30 @@
 
 function solution(arr) {
   let answer = 0;
+  let n = arr.length;
+  let horizontal = [-1, 0, 1, 0];
+  let vertical = [0, 1, 0, -1];
 
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let flag = 1;
+      for (let k = 0; k < 4; k++) {
+        let nx = i + horizontal[k];
+        let ny = j + vertical[k];
+        if (
+          nx >= 0 &&
+          nx < n &&
+          ny >= 0 &&
+          ny < n &&
+          arr[nx][ny] >= arr[i][j]
+        ) {
+          flag = 0;
+          break;
+        }
+      }
+      if (flag) answer++;
+    }
+  }
   return answer;
 }
 
