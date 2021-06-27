8fc4d3e1765cd832e00ecf10f681952f1fd18e05
Mark Yoo <yjinhyuk@gmail.com>
2021-06-27 22:00:08 +0900

    Add: active_palidrome.js

---
+++ active_palindrome.js
@@ -0,0 +1,11 @@
+// 문자열을 검사하되 받은 문자열 내에서 알파벳만을 가지고 검사한다.
+
+function solution(word) {
+  let answer = false;
+
+  return answer;
+}
+
+let word = "got7, BTs, stB, 7ToG";
+let word = "got7, BTs, stB, 7ToG";
+console.log(solution(word));
