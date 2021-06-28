c75798a92c109c32ee374b7883c475906d5368da
Mark Yoo <yjinhyuk@gmail.com>
2021-06-28 22:35:41 +0900

    Update: active_palindrome.js Detail: used RegEx for to remove everything else except alphabet

--- active_palindrome.js
+++ active_palindrome.js
@@ -1,11 +1,14 @@
 // 문자열을 검사하되 받은 문자열 내에서 알파벳만을 가지고 검사한다.
 
 function solution(word) {
-  let answer = false;
+  let answer = true;
+  word = word.toLowerCase().replace(/[^a-z]/g, "");
 
+  if (word.split("").reverse().join("") !== word) return (answer = false);
   return answer;
 }
 
 let word = "got7, BTs, stB, 7ToG";
-let word = "got7, BTs, stB, 7ToG";
+let word2 = "got7, BTs, stB, 7ToG, Palindrome";
 console.log(solution(word));
+console.log(solution(word2));
