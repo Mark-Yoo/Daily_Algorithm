7b82f1bd07b6785948be20c0de8f86f008030408
Mark Yoo <yjinhyuk@gmail.com>
2021-07-07 20:42:33 +0900

    Update: pair_mentor_mentee.js

--- pair_mentor_mentee.js
+++ pair_mentor_mentee.js
@@ -2,19 +2,31 @@
 // 멘토의 등수는 모든 테스트에서 멘티보다 높아야 한다.
 
 function solution(arr) {
-  let answer = [];
+  let answer = 0;
+  let exampleArr = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[0].length; j++) {
+  for (let i = 1; i <= arr[0].length; i++) {
+    for (let j = 1; j <= arr[0].length; j++) {
+      let count = 0;
       for (let k = 0; k < arr.length; k++) {
+        let mentori = 0;
+        let menteej = 0;
+        if (i === j) break;
         for (let l = 0; l < arr[0].length; l++) {
-          if (i === k && j === l) break;
-          if (arr[i][j] < arr[k][l]) console.log("멘티", arr[i][j]);
+          if (arr[k][l] === i) mentori = l;
+          if (arr[k][l] === j) menteej = l;
         }
+        console.log("mentor", i, "mentee", j);
+        console.log("mentorG", mentori, "menteeG", menteej);
+        if (mentori < menteej) count++;
+      }
+      if (count === arr.length) {
+        exampleArr.push([i, j]);
+        console.log(exampleArr);
+        answer++;
       }
     }
   }
-
   return answer;
 }
 
