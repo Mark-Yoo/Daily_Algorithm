c3f3b50767114650ed878c35ac5e4d4262d3bc71
Mark Yoo <yjinhyuk@gmail.com>
2021-07-20 17:43:26 +0900

    Update: find_parentheses.js

--- find_parantheses.js
+++ find_parantheses.js
@@ -1,8 +1,20 @@
 // 괄호의 쌍이 제대로 맞는지 체크하는 함수를 만드시오
-function solution() {
-  let answer;
+function solution(parentheses) {
+  let answer = true;
+  let tempArr = [];
+
+  for (let x of parentheses) {
+    if (tempArr.length === 0 && x === ")") return (answer = false);
+    if (x === "(") {
+      tempArr.push("(");
+    } else {
+      tempArr.pop();
+    }
+  }
+
+  if (tempArr.length) answer = false;
   return answer;
 }
 
-let arr = [];
-console.log(solution(arr));
+let parentheses = "(()(()))(()";
+console.log(solution(parentheses));
