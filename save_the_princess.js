38551167bf0b081b1cef05c652a5fbf976a7d2e7
Mark Yoo <yjinhyuk@gmail.com>
2021-07-23 21:51:00 +0900

    Update: save_the_princess.js

--- save_the_princess.js
+++ save_the_princess.js
@@ -1,7 +1,22 @@
-function solution(prince) {
-  let answer;
+function solution(numberOfPrince, exception) {
+  let answer = 0;
+  let count = 1;
+  let tempArr = Array.from({ length: numberOfPrince }, (v, i) => i + 1);
+
+  while (tempArr.length > 1) {
+    for (let i = 1; i <= exception; i++) {
+      if (count % exception === 0) {
+        tempArr.shift();
+      } else {
+        tempArr.push(tempArr[0]);
+        tempArr.shift();
+      }
+      count++;
+    }
+  }
+
+  answer = tempArr[0];
   return answer;
 }
 
-let prince;
-console.log(solution(prince));
+console.log(solution(8, 3));
