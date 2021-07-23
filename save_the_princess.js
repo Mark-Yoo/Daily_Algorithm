00a791cda12d54091204075257964fe5990c2318
Mark Yoo <yjinhyuk@gmail.com>
2021-07-23 21:59:23 +0900

    Update: function refactored Detail: remove unnecessary variable (count) from function Detail2: used return value of shift method instead of specify index of value from tempArr

--- save_the_princess.js
+++ save_the_princess.js
@@ -1,21 +1,34 @@
+// function solution(numberOfPrince, exception) {
+//   let answer = 0;
+//   let count = 1;
+//   let tempArr = Array.from({ length: numberOfPrince }, (v, i) => i + 1);
+
+//   while (tempArr.length > 1) {
+//     for (let i = 1; i <= exception; i++) {
+//       if (count % exception === 0) {
+//         tempArr.shift();
+//       } else {
+//         tempArr.push(tempArr[0]);
+//         tempArr.shift();
+//       }
+//       count++;
+//     }
+//   }
+
+//   answer = tempArr[0];
+//   return answer;
+// }
+
 function solution(numberOfPrince, exception) {
   let answer = 0;
-  let count = 1;
   let tempArr = Array.from({ length: numberOfPrince }, (v, i) => i + 1);
 
-  while (tempArr.length > 1) {
-    for (let i = 1; i <= exception; i++) {
-      if (count % exception === 0) {
-        tempArr.shift();
-      } else {
-        tempArr.push(tempArr[0]);
-        tempArr.shift();
-      }
-      count++;
-    }
+  while (tempArr.length) {
+    for (let i = 1; i < exception; i++) tempArr.push(tempArr.shift());
+    tempArr.shift();
+    if (tempArr.length === 1) answer = tempArr.shift();
   }
 
-  answer = tempArr[0];
   return answer;
 }
 
