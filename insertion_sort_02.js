f772105360c35ce3df1fe29f261f3a5b00f7f999
Mark Yoo <yjinhyuk@gmail.com>
2021-08-02 22:03:46 +0900

    Update: insertion_sort_02.js

--- insertion_sort_02.js
+++ insertion_sort_02.js
@@ -3,6 +3,16 @@
 function solution(arr) {
   let answer = arr;
 
+  for (let i = 0; i < arr.length; i++) {
+    let temp = arr[i];
+    let j;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > temp) arr[j + 1] = arr[j];
+      else break;
+    }
+    arr[j + 1] = temp;
+  }
+
   return answer;
 }
 
