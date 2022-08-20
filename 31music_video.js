99a6350450bed12d9877037e00c5b2cf9934d4ee
Mark-Yoo <yjinhyuk@gmail.com>
2022-08-20 23:38:47 +0900

    Feat: algorithm modified

--- 31music_video.js
+++ 31music_video.js
@@ -1,7 +1,18 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function count(songs, capacity) {
-    
+    let count = 1
+    let sum = 0;
+
+    for (let x of songs) {
+        if (sum + x > capacity) {
+            count++;
+            sum = x;
+        } else {
+            sum += x;
+        }
+        return count;
+    }
 }
 
 function solution(m, songs) {
@@ -11,6 +22,15 @@ function solution(m, songs) {
 
     while (left <= right) {
         let mid = parseInt((left + right) / 2);
+
+        if (count(songs, mid) <= m) {
+            answer = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+
+        return answer;
     }
     
     return answer;
