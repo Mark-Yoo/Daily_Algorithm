16516a7c86727d6c64b0b7a29af9e21688a75683
Mark-Yoo <yjinhyuk@gmail.com>
2022-09-06 16:28:47 +0600

    Feat: compound number algorithm added

---
+++ 47compound_number.js
@@ -0,0 +1,25 @@
+let arr = [2, 4, 5, 8, 12];
+
+function solution(n, k, arr, m) {
+    let answer = 0;
+    let temp = Array.from({ length: k});
+    
+    function DFS(L, s, sum) {
+        if (L === k) {
+            if (sum % m === 0) {
+                answer++;
+            }
+            console.log(sum, temp);
+        } else {
+            for (let i = s; i < n; i++) {
+                temp[L] = arr[i];
+                DFS(L+1, i+1, sum+arr[i]);
+            }
+        }
+    }
+
+    DFS(0, 0, 0);
+    return answer;
+}
+
+console.log(solution(5, 3, arr, 6));
