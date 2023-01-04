ae2b4c2a1d3461c68ffae4c0584dc82900ad8145
Mark-Yoo <yjinhyuk@gmail.com>
2023-01-04 20:14:55 +0900

    feat : get prime num algorithm added (not an efficient way)

---
+++ Programmers/prime_num_case.js
@@ -0,0 +1,19 @@
+// Not efficient way with simple loop statement
+function isPrimeNum(num) {
+    for (let i = 2; i < num; i ++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function solution(n) {
+    let answer = 0;
+    for (let i = 2; i <= n; i ++) {
+        if (isPrimeNum(i)) {
+            answer += 1;
+        }
+    }
+    return answer;
+}
