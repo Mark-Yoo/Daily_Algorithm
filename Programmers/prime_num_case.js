897fabf573c47d7cc60a543abdd41bedbda94d0d
Mark-Yoo <yjinhyuk@gmail.com>
2023-01-05 20:52:52 +0900

    feat : Way to skip calculating since square root added

--- Programmers/prime_num_case.js
+++ Programmers/prime_num_case.js
@@ -17,3 +17,23 @@ function solution(n) {
     }
     return answer;
 }
+
+// Stop calculating since square root
+function isPrimeNum(num) {
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function solution(n) {
+    let answer = 0;
+    for (let i = 2; i <= n; i++) {
+        if (isPrimeNum(i)) {
+            answer += 1;
+        }
+    }
+    return answer;
+}
