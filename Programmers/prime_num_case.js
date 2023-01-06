34fd593b5bce4a02246b18a61047460cad58a106
Mark-Yoo <yjinhyuk@gmail.com>
2023-01-06 21:41:23 +0900

    feat : Eratosthenes' sieve way added

--- Programmers/prime_num_case.js
+++ Programmers/prime_num_case.js
@@ -37,3 +37,20 @@ function solution(n) {
     }
     return answer;
 }
+
+function getPrimeNum(num) {
+    const prime = [false, false, ...Array(num - 1).fill(true)];
+
+    for (let i = 2; i * i <= num; i +=+) {
+        if (prime[i]) {
+            for (let j = i * 2; j <= num; j++) {
+                prime[j] = false;
+            }
+        }
+    }
+    return prime.filter(Boolean);
+}
+
+function solution(num) {
+    return getPrimeNum(num).length;
+}
