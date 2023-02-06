856c7ce7eb1a632bbe5901c74a8bcf3b44be1b76
Mark-Yoo <yjinhyuk@gmail.com>
2023-02-06 21:48:57 +0900

    feat : modified version of prev function added detail : modified version only calculates half times from functions above

--- 70find_right_weapon.js
+++ 70find_right_weapon.js
@@ -18,3 +18,29 @@ function solution(number, limit, power) {
     
     return answer;
 }
+
+// 2. make caculation occurs only half amount from the function above
+function solution(number, limit, power) {
+    let answer = 0;
+    
+    for (let i = 1; i <= number; i++) {
+        let tempNum = 0;
+        for (let j = 1; j <= Math.sqrt(i); j++) {
+            if (!(i % j)) {
+                if (i / j === j) {
+                    tempNum += 1;
+                } else {
+                    tempNum += 2;
+                }
+            }
+            
+            if (tempNum > limit) {
+                tempNum = power;
+                break;
+            }
+        }
+        answer += tempNum;
+    }
+    
+    return answer;
+}
