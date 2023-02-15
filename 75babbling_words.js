49a2e8c731e70702e56ad2d5f3de27aa4226631b
Mark-Yoo <yjinhyuk@gmail.com>
2023-02-15 09:19:34 +0900

    feat : solution which only can solve one condition added

--- 75babbling_words.js
+++ 75babbling_words.js
@@ -15,3 +15,26 @@ function solution(babbling) {
     
     return answer;
 }
+
+// Fulfill one condition
+
+function solution(babbling) {
+    let answer = 0;
+    let wordList = ["aya", "ye", "woo", "ma"];
+    let tempList = [...babbling];
+    let tempWord = "";
+    
+    for (let i = 0; i < babbling.length; i++) {
+        for (let j = 0; j < wordList.length; j++) {
+            if (tempList[i].includes(wordList[j])) {
+                tempList[i] = tempList[i].replace(wordList[j], "");
+                
+                if (tempList[i].length === 0) {
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
