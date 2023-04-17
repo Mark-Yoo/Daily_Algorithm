3822e008197b115c8139ef59e3e0aae1f6185e3d
Mark-Yoo <yjinhyuk@gmail.com>
2023-04-17 10:00:38 +0900

    feat : aritostenesse net algorithm added

--- 106aritostenes_net.js
+++ 106aritostenes_net.js
@@ -16,3 +16,32 @@ function solution(n) {
     }
     return answer;
 }
+
+
+// 2. working solution for aritostenesse net algorithm
+function solution(n) {
+    let answer = 0;
+    let tempArr = [];
+    let root = Math.sqrt(n);
+    
+    for (let i = 0; i <= n; i++) {
+        tempArr.push(true);
+    }
+    
+    tempArr[0] = false;
+    tempArr[1] = false;
+    
+    for (let j = 2; j < root; j++) {
+        if (tempArr[j]) {
+            for (let k = j * j; k <= n; k += j) {
+                tempArr[k] = false;
+            }
+        }
+    }
+    
+    for (let l = 0; l < tempArr.length; l++) {
+        if (tempArr[l]) answer += 1;
+    }
+    
+    return answer;
+}
