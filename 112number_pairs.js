b126b5d81a6fe22435b66c4bfb930692afae9617
Mark-Yoo <yjinhyuk@gmail.com>
2023-04-27 09:23:17 +0900

    feat : number pairs working solution added

--- 112number_pairs.js
+++ 112number_pairs.js
@@ -1,3 +1,4 @@
+// HOF function cause delay
 function solution(X, Y) {
     let answer = "";
     let tempStr = [];
@@ -13,6 +14,31 @@ function solution(X, Y) {
     if (!tempStr.length) return answer = "-1";
     answer = tempStr.sort((a, b) => b - a).join("");
     
+    if (answer[0] === "0") return answer = "0";
+    
+    return answer;
+}
+
+// Working solution for long string
+function solution(X, Y) {
+    let answer = "";
+    let tempStr = [];
+    let xArr = new Array(10).fill(0);
+    let yArr = new Array(10).fill(0);
+    
+    X.split("").forEach(n => xArr[n] = xArr[n] + 1);
+    Y.split("").forEach(n => yArr[n] = yArr[n] + 1);
+    
+    for (let i = 0; i < xArr.length; i++) {
+        const count = Math.min(xArr[i], yArr[i]);
+        
+        for (let j = 0; j < count; j++) {
+            tempStr.push(i);
+        }
+    }
+    
+    if (!tempStr.length) return answer = "-1";
+    answer = tempStr.sort((a, b) => b - a).join("");
     if (answer[0] === "0") return answer = "0";
     
     return answer;
